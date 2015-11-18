93ca6405db38102262c159e18675edd696076f4f
Brennon Loveless <brennon.loveless@gmail.com>
2015-11-17 20:14:12 -0700

    Brandon said to PUSH

--- mm.c
+++ mm.c
@@ -22,17 +22,17 @@
  * NOTE TO STUDENTS: Before you do anything else, please
  * provide your identifying information in the following struct.
  ********************************************************/
-team_t team = {
+ team_t team = {
     /* Team name */
-    "349",
+  "349",
     /* First member's full name */
-    "Brennon Loveless",
+  "Brennon Loveless",
     /* First member's UID */
-    "u0550904",
+  "u0550904",
     /* Second member's full name (leave blank if none) */
-    "Brandon Sara",
+  "Brandon Sara",
     /* Second member's UID (leave blank if none) */
-    "u0516100"
+  "u0516100"
 };
 
 /* single word (4) or double word (8) alignment */
@@ -41,15 +41,27 @@ team_t team = {
 /* rounds up to the nearest multiple of ALIGNMENT */
 #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
 
+typedef struct block_meta {
+  size_t size;
+  struct block_meta *next;
+  int free;
+  // int magic; // For debugging only. TODO: remove this in non-debug mode.
+} block_meta;
 
-#define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
+#define SIZE_T_SIZE (ALIGN(sizeof(block_meta)))
+
+block_meta *global_base = NULL;
+
+block_meta *find_free_block(block_meta **last, size_t size);
+block_meta *get_block_ptr(void *ptr);
+block_meta *request_space(block_meta* last, size_t size);
 
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
-    return 0;
+  return 0;
 }
 
 /* 
@@ -58,14 +70,37 @@ int mm_init(void)
  */
 void *mm_malloc(size_t size)
 {
-    int newsize = ALIGN(size + SIZE_T_SIZE);
-    void *p = mem_sbrk(newsize);
-    if (p == (void *)-1)
-        return NULL;
-    else {
-        *(size_t *)p = size;
-        return (void *)((char *)p + SIZE_T_SIZE);
+  block_meta *block;
+  int newsize = ALIGN(size + SIZE_T_SIZE);
+  // void *p = mem_sbrk(newsize);
+
+  if(size <= 0) {
+    return NULL;
+  }
+
+  if(!global_base) {
+    block = request_space(NULL, newsize);
+    if(!block) {
+      return NULL;
     }
+
+    global_base = block;
+  }
+  else {
+    block_meta *last = global_base;
+    block = find_free_block(&last, newsize);
+    if(!block) {
+      block = request_space(last, newsize);
+      if(!block) {
+        return NULL;
+      }
+    }
+    else {
+      block->free = 0;
+    }
+  }
+
+  return (block+1);
 }
 
 /*
@@ -80,31 +115,68 @@ void mm_free(void *ptr)
  */
 void *mm_realloc(void *ptr, size_t size)
 {
-    void *oldptr = ptr;
-    void *newptr;
-    size_t copySize;
-    
-    newptr = mm_malloc(size);
-    if (newptr == NULL)
-      return NULL;
-    copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
-    if (size < copySize)
-      copySize = size;
-    memcpy(newptr, oldptr, copySize);
-    mm_free(oldptr);
-    return newptr;
+  void *oldptr = ptr;
+  void *newptr;
+  size_t copySize;
+
+  newptr = mm_malloc(size);
+
+  if (newptr == NULL) {
+    return NULL;
+  }
+
+  copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
+
+  if (size < copySize) {
+    copySize = size;
+  }
+
+  memcpy(newptr, oldptr, copySize);
+  mm_free(oldptr);
+
+  return newptr;
 }
 
 
 
 
+block_meta *find_free_block(block_meta **last, size_t size)
+{
+  block_meta *current = global_base;
 
+  while (current && !(current->free && current->size >= size)) {
+    *last = current;
+    current = current->next;
+  }
 
+  return current;
+}
 
+block_meta *get_block_ptr(void *ptr)
+{
+  return (block_meta*)ptr - 1;
+}
 
+block_meta *request_space(block_meta* last, size_t size)
+{
+  block_meta *block;
+  block = mem_sbrk(0);
+  void *request = mem_sbrk(size);
+  // assert((void*)block == request); // Not thread safe.
+  if (request == (void*) -1) {
+    return NULL; // sbrk failed.
+  }
 
+  if (last) { // NULL on first request.
+    last->next = block;
+  }
 
-
+  block->size = size;
+  block->next = NULL;
+  block->free = 0;
+  // block->magic = 0x12345678;
+  return block;
+}
 
 
 
